1aec0ee346afcad0638164162c7c50762ffa7341
Christopher J. McClellan <ckuhn203@gmail.com>
2015-07-26 13:32:00 -0400

    threw it all away and started over

---
+++ Blink/Blink/Blink.c
@@ -0,0 +1,31 @@
+/*
+* Blink.c
+*
+* Created: 7/26/2015 11:29:20 AM
+*  Author: Christopher
+*/
+
+
+#include <avr/io.h>
+#include <stdint.h>
+
+void delay(volatile uint32_t d)
+{
+    while (d-- != 0)     // loops while non-0 and decrements
+    ;
+}
+
+int main(void)
+{
+    PORTE_DIR = 0b11111111; //Configure PORTE (LEDS) for output
+    PORTE_OUT = 0b11111111; //LEDs are active low, this makes sure they're off on start up
+    
+    while(1)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            PORTE_OUT = ~(1 << i);
+            delay(1000);
+        }
+    }
+}
