45a1b7a6e725bf0d0824caa1c8533205c98444dc
Christopher J. McClellan <ckuhn203@gmail.com>
2015-08-01 18:41:31 -0400

    fixed bad baud rate in flasher.bat

--- flasher.bat
+++ flasher.bat
@@ -1,1 +1,3 @@
-Batchisp -device ATXMEGA128A1 -hardware RS232 -port COM3 -baudrate 11500 -operation onfail abort memory flash erase f blankcheck loadbuffer "C:\Users\Christopher\Documents\Atmel Studio\6.2\XMega-A1-XPlored\Blink\Blink\Release\blink.hex" program verify start reset 0
+Batchisp -device ATXMEGA128A1 -hardware RS232 -port COM3 -baudrate 115200 -operation onfail abort memory flash erase f blankcheck loadbuffer "C:\Users\Christopher\Documents\Atmel Studio\6.2\XMega-A1-XPlored\Blink\Blink\Release\blink.hex" program verify start reset 0
+
+pause
