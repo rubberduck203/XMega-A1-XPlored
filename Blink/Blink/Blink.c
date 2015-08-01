bf9f4831f6f0daac691ddd8a3fba615efad62fc6
Christopher J. McClellan <ckuhn203@gmail.com>
2015-08-01 18:44:54 -0400

    Light runner works on a timer interrupt now
    Needs clean up, but this is a good working state

--- Blink/Blink/Blink.c
+++ Blink/Blink/Blink.c
@@ -6,8 +6,10 @@
 */
 
 #include <stdint.h>
-#include "led.h"
 #include <stdbool.h>
+#include <avr/interrupt.h>
+
+#include "led.h"
 
 #ifdef DEBUG
     #define DELAYITERATIONS 0
@@ -15,12 +17,50 @@
     #define DELAYITERATIONS 10000
 #endif
 
+#ifdef DEBUG
+    #define CLOCK TC_CLKSEL_DIV1_gc
+#else 
+    #define CLOCK TC_CLKSEL_DIV64_gc
+#endif
+
+volatile uint8_t currentLightPosition = 0;
+
+ISR(TCC0_OVF_vect)
+{
+    if (currentLightPosition > 7)
+    {
+       currentLightPosition = 0;
+    }
+    
+    LEDPORT = ~(1 << currentLightPosition);
+    
+    currentLightPosition++;
+}
+
 int main(void)
 {
     initLights();
+        
+	// Set up Timer/Counter 0.
+    
+    // Default System Clock is 2MHz; (2,000,000ticks/sec)
+    // 65,535 ticks in default Period (16 bit integer)
+    // Clock/Prescaler/Period = Light Cycles/sec
+    // 2,000,000/64/65,535 = 31,250/65,535 = 1 overflow/0.4768444 secs =~ 2 overflows/sec
+    // TODO: get a more precise 2Hz lights cycle (use a compare or a different period?)
+    
+    TCC0.CTRLA = (TCC0.CTRLA & ~TC0_CLKSEL_gm) | CLOCK;
+    //TCC0.CTRLA = (TCC0.CTRLA & ~TC0_CLKSEL_gm) | TC_CLKSEL_DIV64_gc; 
+	TCC0.INTCTRLA = ( TCC0.INTCTRLA & ~TC0_OVFINTLVL_gm ) | TC_OVFINTLVL_LO_gc; // Enable overflow interrupt.
+    
+    //enable low level interrupts
+    PMIC.CTRL |= PMIC_LOLVLEN_bm; //0b00000001
+    // enable global interrupts:
+    sei();
     
     while(true)
-    {
-        blinkInSequence(DELAYITERATIONS);
+    {   
+        //blinkInSequence(DELAYITERATIONS);
     }
 }
+
